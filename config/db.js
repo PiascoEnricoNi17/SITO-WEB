ff0771ec52ab04d1adeb7154bc0963c4cc580466
Piasco Enrico <139145587+PiascoEnricoNi17@users.noreply.github.com>
2025-05-27 16:58:39 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
     // Utilizziamo le variabili d'ambiente per la connessione a MongoDB
     // Se non sono presenti, usiamo la connessione locale di default
-    const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/loraAirDB';
+    const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/loraAirDB'; // QUI VA MESSO: const mongoURI = 'mongodb+srv://doadmin:6BsQ94q83wo02r1A@dbaas-db-8396718-38526fb0.mongo.ondigitalocean.com/loraAirDB?tls=true&authSource=admin';
     
     const conn = await mongoose.connect(mongoURI, {
       // Parametri opzionali di configurazione se necessari
